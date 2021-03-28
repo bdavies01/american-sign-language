fa445ca0d8f4c12e58c9a6b756a70cd6bebf0a90
bdavies01 <bertdaviezz@gmail.com>
2021-03-28 13:30:51 -0700

    update readme

--- ASL.py
+++ ASL.py
@@ -3,14 +3,7 @@
 # Commented out IPython magic to ensure Python compatibility.
 # %matplotlib inline
 
-"""## Question 1 - Gesture Recognition using Convolutional Neural Networks
----
-American Sign Language (ASL) is a complete, complex language that employs signs made by moving the hands combined with facial expressions and postures of the body. It is the primary language of many North Americans who are deaf and is one of several communication options used by people who are deaf or hard-of-hearing.
-
-The hand gestures representing English alphabet are shown below. In this question, you will focus on classifying these hand gesture images using convolutional neural networks. Specifically, given an image of a hand showing one of the letters, we want to detect which letter is being represented.
-
-Run the following code cell to download the training and test data. It might take a while to download the zip file and extract it.
-"""
+# Gesture Recognition using Convolutional Neural Networks
 
 import os
 import shutil
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# american-sign-language
+# Gesture Recognition using Convolutional Neural Networks
+  
+American Sign Language (ASL) is a complete, complex language that employs signs made by moving the hands combined with facial expressions and postures of the body. It is the primary language of many North Americans who are deaf and is one of several communication options used by people who are deaf or hard-of-hearing.  
+
+The hand gestures representing English alphabet are shown below. This project focuses on classifying these hand gesture images using convolutional neural networks. Specifically, given an image of a hand showing one of the letters, we want to detect which letter is being represented.  
+![alt text] (https://drive.google.com/uc?id=1nRxq6yqDkmumUuePXfDx_5YgGl9vKXcj)
